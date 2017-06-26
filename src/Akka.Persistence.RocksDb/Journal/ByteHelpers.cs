c1a9b37279abdc25ac98ffe7f80d20306464c960
alexvaluyskiy <yurisikorsky@gmail.com>
2017-06-26 13:51:17 +0300

    Fixed endianess bug

---
+++ src/Akka.Persistence.RocksDb/Journal/ByteHelpers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+
+namespace Akka.Persistence.RocksDb.Journal
+{
+    public static class ByteHelpers
+    {
+        public static int GetInt(byte[] source, int offset = 0)
+        {
+            return IPAddress.NetworkToHostOrder(BitConverter.ToInt32(source, offset));
+        }
+
+        public static long GetLong(byte[] source, int offset = 0)
+        {
+            return IPAddress.NetworkToHostOrder(BitConverter.ToInt64(source, offset));
+        }
+
+        public static byte[] PutInt(this byte[] target, int x, int offset = 0)
+        {
+            target[offset + 0] = (byte)(x >> 24);
+            target[offset + 1] = (byte)(x >> 16);
+            target[offset + 2] = (byte)(x >> 8);
+            target[offset + 3] = (byte)(x >> 0);
+
+            return target;
+        }
+
+        public static byte[] PutLong(this byte[] target, long x, int offset = 0)
+        {
+            target[offset + 0] = (byte)(x >> 56);
+            target[offset + 1] = (byte)(x >> 48);
+            target[offset + 2] = (byte)(x >> 40);
+            target[offset + 3] = (byte)(x >> 32);
+            target[offset + 4] = (byte)(x >> 24);
+            target[offset + 5] = (byte)(x >> 16);
+            target[offset + 6] = (byte)(x >> 8);
+            target[offset + 7] = (byte)(x >> 0);
+
+            return target;
+        }
+    }
+
+    public enum ByteOrder
+    {
+        BigEndian,
+        LittleEndian
+    }
+}
--- src/Akka.Persistence.RocksDb/Journal/RocksDbKey.cs
+++ src/Akka.Persistence.RocksDb/Journal/RocksDbKey.cs
@@ -23,18 +23,18 @@ namespace Akka.Persistence.RocksDb.Journal
     {
         public static byte[] KeyToBytes(Key key)
         {
-            var bb = ByteBuffer.Allocate(20);
-            bb.Put(BitConverter.GetBytes(key.PersistenceId));
-            bb.Put(BitConverter.GetBytes(key.SequenceNr));
-            bb.Put(BitConverter.GetBytes(key.MappingId));
-            return bb.Array();
+            var bytes = new byte[16];
+            bytes.PutInt(key.PersistenceId);
+            bytes.PutLong(key.SequenceNr, 4);
+            bytes.PutInt(key.MappingId, 4 + 8);
+            return bytes;
         }
 
         public static Key KeyFromBytes(byte[] bytes)
         {
-            int persistenceId = BitConverter.ToInt32(bytes, 0);
-            long sequenceNr = BitConverter.ToInt64(bytes, 4);
-            int mappingId = BitConverter.ToInt32(bytes, 12);
+            int persistenceId = ByteHelpers.GetInt(bytes, 0);
+            long sequenceNr = ByteHelpers.GetLong(bytes, 4);
+            int mappingId = ByteHelpers.GetInt(bytes, 4 + 8);
             return new Key(persistenceId, sequenceNr, mappingId);
         }
 
@@ -42,12 +42,12 @@ namespace Akka.Persistence.RocksDb.Journal
 
         public static byte[] CounterToBytes(long ctr)
         {
-            var bb = ByteBuffer.Allocate(8);
-            bb.Put(BitConverter.GetBytes(ctr));
-            return bb.Array();
+            var bytes = new byte[8];
+            bytes.PutLong(ctr);
+            return bytes;
         }
 
-        public static long CounterFromBytes(byte[] bytes) => BitConverter.ToInt64(bytes, 0);
+        public static long CounterFromBytes(byte[] bytes) => ByteHelpers.GetLong(bytes, 0);
 
         public static Key MappingKey(int id) => new Key(1, 0L, id);
         public static bool IsMappingKey(Key key) => key.PersistenceId == 1;
